a5dce23781041dd619f22877510130aa9a099536
Mahabub Alam <mahabubx7@gmail.com>
2023-08-29 20:16:47 +0600

    devise(auth): auth-login-register-setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Display from './components/Display';
+import Home from './pages/Index';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Navbar from './components/Navbar';
+import Protected from './components/ProtectedRoute';
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route exact path='/' element={<Display />} />
-      </Routes>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <Protected>
+                <Home />
+              </Protected>
+            }
+          />
+          <Route path='/home' element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route
+            path='*'
+            element={
+              <h2 className='text-red-500 font-semibold'>Page not found!</h2>
+            }
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { register } from '../redux/auth/authSlice';
+
+function Register() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  const [errors, setErrors] = useState([]);
+
+  const handleInput = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: [e.target.value][0],
+    });
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    await axios
+      .post(
+        `${process.env.REACT_APP_API_ENDPOINT}/auth/signup`,
+        { user },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      .then((res) => {
+        dispatch(register(res.data));
+        setErrors([]);
+        navigate('/');
+      })
+      .catch((err) => {
+        setErrors([...errors, err]);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className='text-4xl text-blue-600 font-bold pt-6 py-20'>Register</h1>
+
+      <form
+        onSubmit={handleRegister}
+        onChange={handleInput}
+        className='flex flex-col gap-y-4 bg-white px-4 py-12 shadow-md border-gray-100 border-2 w-[96%] max-w-[640px] mx-auto'
+      >
+        {errors.map((err) => (
+          <span
+            key={String(err.code).codePointAt * Math.random()}
+            className='text-red-500'
+          >
+            {err?.response?.data}
+          </span>
+        ))}
+        <input
+          className='p-2 pl-4 rounded-sm overflow-hidden outline-none border-2 border-gray-200'
+          name='name'
+          type='text'
+          placeholder='Full Name'
+          autoComplete='false'
+        />
+        <input
+          className='p-2 pl-4 rounded-sm overflow-hidden outline-none border-2 border-gray-200'
+          name='email'
+          type='email'
+          placeholder='Email Address'
+          autoComplete='false'
+        />
+        <input
+          className='p-2 pl-4 rounded-sm overflow-hidden outline-none border-2 border-gray-200'
+          name='password'
+          type='password'
+          placeholder='Password'
+          autoComplete='false'
+        />
+        <button
+          type='submit'
+          className='px-12 py-3 text-white bg-blue-500 rounded-sm max-w-[280px] mx-auto block capitalize mt-6 hover:bg-blue-700'
+        >
+          Sign up
+        </button>
+        <p>
+          <span>Already have an account?</span>
+          <Link to='/login' className='pl-2 text-blue-500 font-semibold'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
