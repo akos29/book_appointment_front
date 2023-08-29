bc444966de3418dc20adff89fc2a1f0855ceff50
Nana Vanina <vaninalarissa@gmail.com>
2023-08-29 18:13:20 +0100

    Import Navbarin app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Display from './components/Display';
-
+import Navbar from './components/NavBar';
 function App() {
   return (
     <div className='App'>
+      < Navbar />
       <Routes>
         <Route exact path='/' element={<Display />} />
       </Routes>
