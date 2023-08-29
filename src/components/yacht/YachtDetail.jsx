3c1eda98f801101d18a33c52fb0364a441cb3bec
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-29 23:51:15 +0200

    Update the display Yacht with the necessary style

--- src/components/yacht/YachtDetail.jsx
+++ src/components/yacht/YachtDetail.jsx
@@ -1,48 +1,58 @@
-import React from 'react';
-import './YachtDetail.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getYacht } from '../../redux/yacht/yachtSlice';
 
-function YachtDetail() {
-  const yachtData = {
-    date: '2023-08-23T10:00:00Z',
-    city: 'Miami',
-    user_id: 1,
-    yacht_id: 1,
-    created_at: '2023-08-20T08:00:00Z',
-    updated_at: '2023-08-20T08:00:00Z',
-    image_path: '/path/to/image1.jpg',
-    yacht_name: 'Luxury Cruiser',
-    price: 1500,
-    duration: '4 hours',
-    yacht_model: 'Model X',
-  };
+function YachtDetail({ yachtId, handleView }) {
+  const yacht = useSelector((state) => state.yachts.yacht);
+  const { yachtLoading } = useSelector((state) => state.yachts);
+  const dispatch = useDispatch();
 
-  return (
+  useEffect(() => {
+    if (yacht?.id) {
+      dispatch(getYacht(yachtId));
+    }
+  }, [dispatch, yachtId]);
+
+  return yachtLoading ? (
+    <h1>Loading ...</h1>
+  ) : (
     <div className='p-4'>
       <h2 className='text-2xl font-semibold mb-4'>Yacht Details</h2>
       <div className='border border-gray-300 rounded p-4'>
         <img
-          src={yachtData.image_path}
+          src={yacht?.photo}
           alt='Yacht'
           className='w-full mb-4 rounded-lg'
         />
         <h3 className='text-xl font-semibold mb-2'>
-          {yachtData.yacht_name} - {yachtData.yacht_model}
+          {yacht?.captain_name} - {yacht?.model}
         </h3>
         <p className='text-gray-600 mb-2'>
-          ${yachtData.price} for {yachtData.duration}
+          ${yacht?.price} for {yacht?.user_id}
         </p>
-        <p className='text-gray-600 mb-2'>
-          Available on: {yachtData.date} in {yachtData.city}
-        </p>
+        <p className='text-gray-600 mb-2'>Available on: {yacht?.created_at}</p>
         <button
           type='button'
           className='bg-blue-500 text-white rounded px-4 py-2'
         >
           Make Reserve
         </button>
+        <button
+          className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
+          onClick={() => handleView(false)}
+          type='button'
+        >
+          Back
+        </button>
       </div>
     </div>
   );
 }
+
+YachtDetail.propTypes = {
+  yachtId: PropTypes.string.isRequired,
+  handleView: PropTypes.func.isRequired,
+};
 
 export default YachtDetail;
