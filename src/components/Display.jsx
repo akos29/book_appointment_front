887caa5dc16642abd25fbc721d41d1f4c327644b
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-29 23:49:58 +0200

    Update the list view by adding view button

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchYachts } from '../redux/yacht/yachtSlice';
-import { fetchYatchs } from '../redux/yatch/yatchSlice';
-import ReservationForm from './reservation/ReservationForm';
-import ReservationsList from './reservation/ReservationList';
+import { fetchYachts, getYacht } from '../redux/yacht/yachtSlice';
 import YachtDetail from './yacht/YachtDetail';
 
 function Display() {
@@ -12,44 +9,68 @@ function Display() {
     (store) => store.yachts,
   );
 
+  const [view, setView] = useState(false);
+  const [selectedYachtId, setSelectedYachtId] = useState(null);
+
   useEffect(() => {
     if (!loaded) dispatch(fetchYachts());
   }, [dispatch, loaded]);
 
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading ...</h1>
-      ) : (
-        <>
-          <h1 className='text-4xl text-blue-600 font-bold pt-6 py-20'>
-            Display yachts!
-          </h1>
-          <div className='flex flex-col gap-4 items-center justify-center'>
-            {error ? (
-              <b className='text-red-500'>{String(error)}</b>
-            ) : (
-              <div className='grid place-content-center gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-col-4'>
-                {yachts.map((yatch) => (
-                  <div
-                    key={yatch.id}
-                    className='shadow-md cursor-pointer rounded-sm overflow-hidden flex flex-col gap-2 bg-white pb-4'
-                  >
-                    <img src={yatch.photo} alt='yacht' />
-                    <h4 className='text-xl my-2 capitalize font-semibold text-blue-600'>
-                      {yatch.model}
-                    </h4>
-                    <em>Captain: {yatch.captain_name}</em>
-                    <b>Price: ${`${yatch.price}`}</b>
-                  </div>
-                ))}
+  const handleView = (id) => {
+    if (view) setView(false);
+    else {
+      setView(true);
+      setSelectedYachtId(id);
+      dispatch(getYacht(id));
+    }
+  };
+
+  let content;
+
+  if (loading) {
+    content = <h1>Loading ...</h1>;
+  } else if (view) {
+    content = <YachtDetail yachtId={selectedYachtId} handleView={handleView} />;
+  } else if (error) {
+    content = <b className='text-red-500'>{String(error)}</b>;
+  } else {
+    content = (
+      <>
+        <h1 className='text-4xl text-blue-600 font-bold pt-6 py-20'>
+          Display yachts!
+        </h1>
+        <div className='flex flex-col gap-4 items-center justify-center'>
+          <div className='grid place-content-center gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-col-4'>
+            {yachts.map((yacht) => (
+              <div
+                key={yacht.id}
+                className='shadow-md cursor-pointer rounded-sm overflow-hidden flex flex-col gap-2 bg-white pb-4'
+              >
+                <img src={yacht.photo} alt='yacht' />
+                <h4 className='text-xl my-2 capitalize font-semibold text-blue-600'>
+                  {yacht.model}
+                </h4>
+                <em>Captain: {yacht.captain_name}</em>
+                <b>Price: ${`${yacht.price}`}</b>
+                <button
+                  type='button'
+                  className='btn btn-primary'
+                  id={yacht.id}
+                  onClick={(e) =>
+                    handleView(e.target.id || e.target.parentNode)
+                  }
+                >
+                  View
+                </button>
               </div>
-            )}
+            ))}
           </div>
-        </>
-      )}
-    </div>
-  );
+        </div>
+      </>
+    );
+  }
+
+  return <div>{content}</div>;
 }
 
 export default Display;
