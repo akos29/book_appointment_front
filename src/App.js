c300fc8a9ce93098e145fb95f39229b4f4714f32
Nana Vanina <vaninalarissa@gmail.com>
2023-08-30 12:15:36 +0100

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Display from './components/Display';
 import Navbar from './components/NavBar';
+
 function App() {
   return (
     <div className='App'>
-      < Navbar />
-      <div className="bar">
+      <Navbar />
+      <div className='bar'>
         <Routes>
           <Route exact path='/' element={<Display />} />
           <Route exact path='/add_reservation' element={<Display />} />
           <Route exact path='/reservation' element={<Display />} />
           <Route exact path='/add_yatch' element={<Display />} />
           <Route exact path='/delete_yatch' element={<Display />} />
-
         </Routes>
-
       </div>
-      
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-  <>
-    <nav className="nav-cont">
+function Navbar() {
+  return (
+    <nav className='nav-cont'>
       <h2>Yatch</h2>
       <ul>
-        <li className="nav-item">
-          <NavLink to="/"  active className="nav-item">Yatchs</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/' active className='nav-item'>
+            Yatchs
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to="/add_reservation" >ADD Reservation</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/add_reservation'>ADD Reservation</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to="/reservation" >My Reservation</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/reservation'>My Reservation</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to="/add_yatch" >ADD Yatch</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/add_yatch'>ADD Yatch</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to="/delete_yatch" >DELETE Yatch</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/delete_yatch'>DELETE Yatch</NavLink>
         </li>
       </ul>
     </nav>
-  </>
-);
+  );
+}
 
 export default Navbar;
