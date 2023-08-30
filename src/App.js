4cb751f15bb41328ef9bb27fb25ba67c399b4d8b
Nana Vanina <71019051+vanina123@users.noreply.github.com>
2023-08-30 13:27:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import Display from './components/Display';
 import Navbar from './components/NavBar';
-
-
 function App() {
   return (
     <div className='App'>
@@ -16,7 +13,6 @@ function App() {
           <Route exact path='/reservation' element={<Display />} />
           <Route exact path='/add_yatch' element={<Display />} />
           <Route exact path='/delete_yatch' element={<Display />} />
-
         </Routes>
       </div>
     </div>
