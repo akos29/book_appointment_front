3eb4c58eb37b6e1418a551004f96033980b6e2fa
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-09-01 23:17:32 +0200

    Add style for yacht Reservation

--- src/components/reservation/ReservationForm.jsx
+++ src/components/reservation/ReservationForm.jsx
@@ -52,25 +52,80 @@ function ReservationForm({ yachtId, yachtName }) {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={user.name} disabled />
+    <div className='p-4'>
+      <form onSubmit={handleSubmit} className='max-w-sm mx-auto'>
+        <div className='mb-4'>
+          <label
+            htmlFor='userName'
+            className='block text-gray-700 font-semibold mb-2'
+          >
+            User Name
+          </label>
+          <input
+            type='text'
+            id='userName'
+            value={user.name}
+            disabled
+            className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500'
+          />
+        </div>
 
-        <input type='text' value={yachtName} disabled />
+        <div className='mb-4'>
+          <label
+            htmlFor='yachtName'
+            className='block text-gray-700 font-semibold mb-2'
+          >
+            Yacht Name
+          </label>
+          <input
+            type='text'
+            id='yachtName'
+            value={yachtName}
+            disabled
+            className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500'
+          />
+        </div>
 
-        <input
-          type='date'
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          required
-        />
-        <input
-          type='text'
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          required
-        />
-        <button type='submit'>Reserve</button>
+        <div className='mb-4'>
+          <label
+            htmlFor='date'
+            className='block text-gray-700 font-semibold mb-2'
+          >
+            Date
+          </label>
+          <input
+            type='date'
+            id='date'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500'
+          />
+        </div>
+
+        <div className='mb-4'>
+          <label
+            htmlFor='city'
+            className='block text-gray-700 font-semibold mb-2'
+          >
+            City
+          </label>
+          <input
+            type='text'
+            id='city'
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+            className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500'
+          />
+        </div>
+
+        <button
+          type='submit'
+          className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300'
+        >
+          Reserve
+        </button>
       </form>
     </div>
   );
--- src/components/yacht/YachtDetail.jsx
+++ src/components/yacht/YachtDetail.jsx
@@ -20,59 +20,54 @@ function YachtDetail({ yachtId, handleView }) {
   return showReservationForm ? (
     <ReservationForm yachtId={yachtId} yachtName={yacht.model} />
   ) : (
-    <div className='flex col'>
-      <div className='grow aspect-w-10 aspect-h-9 lg:aspect-none'>
+    <div className='flex flex-row space-y-9 space-x-9'>
+      <div className='grow aspect-w-16 aspect-h-9 lg:aspect-none self-end'>
         <img
           src={yacht?.photo}
-          alt='Yacht'
-          className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+          alt={yacht?.model}
+          className='w-full object-center object-cover lg:w-full lg:h-full rounded-3xl shadow-2xl'
         />
       </div>
 
-      <div>
-        <h2 className='text-2xl font-semibold mb-4 flex-none w-14 h-14'>
-          Yacht Details
-        </h2>
-        <table className='hover:table-fixed'>
-          <tr>
-            <th className='border-spacing: 1.75rem var(--tw-border-spacing-y)'>
-              Price
-            </th>
-            <td className='border-spacing: 1.75rem var(--tw-border-spacing-y)'>
-              {yacht.price}
-            </td>
-          </tr>
-          <tr>
-            <th>Total Amount Payable</th>
-            <td>{yacht.price - 0.25 * yacht.price}</td>
-          </tr>
-          <tr>
-            <th>Duration</th>
-            <td>1961</td>
-          </tr>
-        </table>
-        <h3 className='text-xl font-semibold mb-2'>
-          {yacht?.captain_name} - {yacht?.model}
-        </h3>
-        <p className='text-gray-600 mb-2'>
-          ${yacht?.price} for {yacht?.user_id}
-        </p>
-        <p className='text-gray-600 mb-2'>Available on: {yacht?.created_at}</p>
-        <div className='flex space-x-2'>
-          <button
-            type='button'
-            className='bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
-            onClick={() => setShowReservationForm(true)}
-          >
-            Make Reserve
-          </button>
-          <button
-            className='bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-300'
-            onClick={() => handleView(false)}
-            type='button'
-          >
-            Back
-          </button>
+      <div className='flex flex-row justify-end'>
+        <div className='flex flex-col justify-between'>
+          <div className='flex flex-col space-y-2'>
+            <h2 className='text-2xl font-semibold h-14 subpixel-antialiased'>
+              Yacht Details
+            </h2>
+            <table className='hover:table-fixed border-spacing-1 transition-colors max-h-4 justify-items-stretch caption-top'>
+              <tr className='border border-slate-600 '>
+                <th className='justify-self-end'>Price</th>
+                <td className='border-spacing: 1.75rem var(--tw-border-spacing-y) justify-self-end'>
+                  {yacht.price}
+                </td>
+              </tr>
+              <tr>
+                <th>Total Amount Payable</th>
+                <td>{yacht.price - 0.25 * yacht.price}</td>
+              </tr>
+              <tr>
+                <th>Duration</th>
+                <td>1961</td>
+              </tr>
+            </table>
+          </div>
+          <div className='flex space-x-2 justify-end'>
+            <button
+              type='button'
+              className='bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
+              onClick={() => setShowReservationForm(true)}
+            >
+              Make Reserve
+            </button>
+            <button
+              className='bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-300'
+              onClick={() => handleView(false)}
+              type='button'
+            >
+              Back
+            </button>
+          </div>
         </div>
       </div>
     </div>
