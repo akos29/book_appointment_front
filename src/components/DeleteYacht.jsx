29860622bb756f8b6ab087d017ef9a510b0cc3f4
Lugard Agu <agulugard@gmail.com>
2023-09-03 16:27:08 +0100

    Declare a function DeleteYacht

--- src/components/DeleteYacht.jsx
+++ src/components/DeleteYacht.jsx
@@ -1,3 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchYachts, deleteYacht } from '../redux/yacht/yachtSlice';
+
+function DeleteYacht() {}
+export default DeleteYacht;
