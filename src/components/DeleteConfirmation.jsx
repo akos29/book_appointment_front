fd79118cda672da571715a40b8e0da374029dea7
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-09-03 21:15:52 +0200

    Update add reservation from nav to appear on modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -4,18 +4,20 @@ function DeleteConfirmation({ isOpen, onCancel, onConfirm }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white p-4 rounded shadow-md w-64">
-        <p className="mb-4">Are you sure you want to delete this reservation?</p>
-        <div className="flex justify-end">
+    <div className='fixed inset-0 flex items-center justify-center z-50'>
+      <div className='bg-white p-4 rounded shadow-md w-64'>
+        <p className='mb-4'>
+          Are you sure you want to delete this reservation?
+        </p>
+        <div className='flex justify-end'>
           <button
-            className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+            className='bg-red-500 text-white px-4 py-2 rounded mr-2'
             onClick={onConfirm}
           >
             Confirm
           </button>
           <button
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+            className='bg-gray-300 text-gray-700 px-4 py-2 rounded'
             onClick={onCancel}
           >
             Cancel
