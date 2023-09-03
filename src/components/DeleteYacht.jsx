69fd65098dfe29f3bb586d1205bd34d8775ffa1e
Lugard Agu <agulugard@gmail.com>
2023-09-03 16:33:47 +0100

    Initialize dispatch

--- src/components/DeleteYacht.jsx
+++ src/components/DeleteYacht.jsx
@@ -2,5 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchYachts, deleteYacht } from '../redux/yacht/yachtSlice';
 
-function DeleteYacht() {}
+function DeleteYacht() {
+  const dispatch = useDispatch();
+  const { yachts, loading, error, loaded } = useSelector(
+    (store) => store.yachts,
+  );
+}
 export default DeleteYacht;
