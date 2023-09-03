251e46261ec7d12e653ee2693cf07795cb649672
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-09-03 22:11:48 +0200

    Fix Linter errors

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function DeleteConfirmation({ isOpen, onCancel, onConfirm }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white p-4 rounded shadow-md w-64">
-        <p className="mb-4">Are you sure you want to delete this reservation?</p>
-        <div className="flex justify-end">
+    <div className='fixed inset-0 flex items-center justify-center z-50'>
+      <div className='bg-white p-4 rounded shadow-md w-64'>
+        <p className='mb-4'>
+          Are you sure you want to delete this reservation?
+        </p>
+        <div className='flex justify-end'>
           <button
-            className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+            type='button'
+            className='bg-red-500 text-white px-4 py-2 rounded mr-2'
             onClick={onConfirm}
           >
             Confirm
           </button>
           <button
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+            type='button'
+            className='bg-gray-300 text-gray-700 px-4 py-2 rounded'
             onClick={onCancel}
           >
             Cancel
@@ -25,5 +30,15 @@ function DeleteConfirmation({ isOpen, onCancel, onConfirm }) {
     </div>
   );
 }
+
+DeleteConfirmation.defaultProps = {
+  isOpen: true,
+};
+
+DeleteConfirmation.propTypes = {
+  isOpen: PropTypes.bool,
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
 
 export default DeleteConfirmation;
