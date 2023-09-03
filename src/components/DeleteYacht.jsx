8977bedbad13a1a090de83721b017f829b47d212
Lugard Agu <agulugard@gmail.com>
2023-09-03 17:51:43 +0100

    Add styling

--- src/components/DeleteYacht.jsx
+++ src/components/DeleteYacht.jsx
@@ -14,22 +14,34 @@ function DeleteYacht() {
 
   return (
     <div>
-      <h1>Delete Yacht!</h1>
+      <h1 className='text-4xl text-red-600 font-bold pt-6 py-20'>
+        Delete Yacht!
+      </h1>
       {loading ? (
         <h1>Loading ...</h1>
       ) : (
-        <div>
+        <div className='flex flex-col gap-4 items-center justify-center'>
           {error ? (
-            <b>{String(error)}</b>
+            <b className='text-red-500'>{String(error)}</b>
           ) : (
-            <div>
+            <div className='grid place-content-center gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-col-4'>
               {yachts.map((yatch) => (
-                <div>
+                <div
+                  key={yatch.id}
+                  className='shadow-md cursor-pointer rounded-sm overflow-hidden flex flex-col gap-2 bg-white pb-4'
+                >
                   <img src={yatch.photo} alt='yacht' />
-                  <h4>{yatch.model}</h4>
+                  <h4 className='text-xl my-2 capitalize font-semibold text-blue-600'>
+                    {yatch.model}
+                  </h4>
                   <em>Captain: {yatch.captain_name}</em>
                   <b>Price: ${`${yatch.price}`}</b>
-                  <button type='button'>Delete</button>
+                  <button
+                    type='button'
+                    className='bg-gray-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded max-w-[50%] mx-auto items-center justify-center'
+                  >
+                    Delete
+                  </button>
                 </div>
               ))}
             </div>
