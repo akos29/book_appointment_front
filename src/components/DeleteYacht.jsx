b81d6c4589d57f676f4e46530ea95af3d6c0d894
Lugard Agu <agulugard@gmail.com>
2023-09-04 15:25:19 +0100

    Import DeleteConfirmation

--- src/components/DeleteYacht.jsx
+++ src/components/DeleteYacht.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchYachts, deleteYacht } from '../redux/yacht/yachtSlice';
+import DeleteConfirmation from '../DeleteConfirmation';
 
 function DeleteYacht() {
   const dispatch = useDispatch();
