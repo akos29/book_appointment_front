e1ade602e95188d43dc5905b8407849754e48acb
Lugard Agu <agulugard@gmail.com>
2023-09-05 14:32:29 +0100

    Modify yachtsSlice

--- src/redux/yacht/yachtSlice.js
+++ src/redux/yacht/yachtSlice.js
@@ -16,6 +16,7 @@ export const fetchYachts = createAsyncThunk('yachts/fetchYachts', async () => {
   try {
     const response = await axios.get(
       `${process.env.REACT_APP_API_ENDPOINT}/yachts`,
+      { params: { is_deleted: false } },
     );
     return response.data;
   } catch (error) {
